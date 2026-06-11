98ba7fe66790aa3c7ba6668616af1226afcbac0f
Karunya Kalkhundiya <143082723+KarunyaKalk@users.noreply.github.com>
2026-06-11 05:11:18 +0000

    Update bitmanipulation solution with hammingWeight

--- bitmanipulation.cpp
+++ bitmanipulation.cpp
@@ -9,3 +9,15 @@ public:
       return c;  
     }
 };
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+      int c=0;
+      while(n){
+        n=n&n-1;
+        c++;
+      }
+      return c;  
+    }
+};
