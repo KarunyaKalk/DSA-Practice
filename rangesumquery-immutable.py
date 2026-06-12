f4319884e6d9a70c70d2b92d6aaf2ed14e3b8c75
Karunya Kalkhundiya <143082723+KarunyaKalk@users.noreply.github.com>
2026-06-12 04:38:31 +0000

    Add rangesumquery-immutable.py solution

---
+++ rangesumquery-immutable.py
@@ -0,0 +1,12 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = [0]
+        for i in nums:
+            x=self.prefix[-1]+i
+            self.prefix.append(x)
+        
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix[right+1]-self.prefix[left]
+        
