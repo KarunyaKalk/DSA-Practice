622459fef3e528ce3196bee8432c0f4836567675
Karunya Kalkhundiya <143082723+KarunyaKalk@users.noreply.github.com>
2026-06-12 07:08:10 +0000

    Add rangesumequery2D.py

---
+++ rangesumequery2D.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class NumMatrix:
+
+    def __init__(self, matrix: List[List[int]]):
+        n,m=len(matrix),len(matrix[0])
+        self.p=[[0]*(m+1) for i in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                self.p[i][j]=matrix[i-1][j-1]+self.p[i-1][j]+self.p[i][j]+self.p[i][j-1]-self.p[i-1][j-1]
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.p[row2+1][col2+1]-self.p[row1][col2+1]-self.p[row2+1][col1]+self.p[row1][col1]
